abd4ddefda985e0686b90499a7e568c85462239f
ignalpha4 <shubhambankar444@gmail.com>
2023-04-25 02:21:11 +0530

    L6 completed

---
+++ L6/decimal_to_binary.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    int i = 0;
+    while (n != 0)
+    {
+        int digit = n % 10; // we are removing the last digit for eg=101  using this we get 1
+
+        if (digit == 1)
+        {
+            ans = ans + pow(2, i); // if 1 then add the 2^i to ans
+        }
+        n = n / 10; // to remove last element for eg 101 o/p 10
+        i++;        // to keep track of raise to power
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
